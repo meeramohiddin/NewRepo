f98dcbcb14cc0967baa2817ea243a2c46b5631f3
Meera Mohiddin Sheik <meeramohiddin.s@valuelabs.com>
2025-05-05 21:26:22 +0800

    meera

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
-# Configure the AWS Provider
-provider "aws" {
-  version = "~> 5.0"
-  region  = "us-east-1"
+terraform {
+  required_version = "~> 1.0" 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
-# Create a VPC
-resource "aws_vpc" "meera" {
-  cidr_block = "10.0.0.0/16"
+
+provider "aws" {
+  region = var.aws_region
+  #profile = "default"
 }
